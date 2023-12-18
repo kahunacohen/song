4152634c253f0cd32ff2e9e420406852eb5d1941
Aaron Cohen <aaroncohendev@gmail.com>
2023-12-18 21:48:51 +0200

    schema

--- db/migrations/000001_init_tables.up.sql
+++ db/migrations/000001_init_tables.up.sql
@@ -22,18 +22,28 @@ CREATE TABLE performers (
     created_at TIMESTAMP DEFAULT current_timestamp,
     updated_at TIMESTAMP DEFAULT current_timestamp
 );
-CREATE TYPE genre AS ENUM ('Bluegrass', 'Blues', 'Rock', 'Pop', 'Hip-Hop', 'Country', 'Traditional');
+CREATE TYPE genre AS ENUM ('Bluegrass', 'Blues', 'Rock', 'Pop', 'Hip-Hop', 'Country', 'Traditional', 'Folk');
 
 CREATE TABLE songs (
     song_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     "key" VARCHAR(5),
     genre genre NOT NULL,
-    capo VARCHAR(20),
+    capo SMALLINT,
     lyrics TEXT NOT NULL,
-    composer_id INT REFERENCES composers(composer_id) ON DELETE CASCADE,
-    performer_id INT REFERENCES performers(performer_id) ON DELETE CASCADE,
     user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT current_timestamp,
     updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE composer_song (
+    composer_id INT REFERENCES composers(composer_id),
+    song_id INT REFERENCES songs(song_id),
+    PRIMARY KEY (composer_id, song_id)
+);
+
+CREATE TABLE performer_song (
+    performer_id INT REFERENCES performers(performer_id),
+    song_id INT REFERENCES songs(song_id),
+    PRIMARY KEY (performer_id, song_id)
 );
--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,13 +1,28 @@
-INSERT INTO users (username, email, password) 
+INSERT INTO users (first_name, last_name, email, password) 
 VALUES 
-    ('user1@example.com', 'password1'),
-    ('user2', 'user2@example.com', 'password2');
+    ('John', 'Doe', 'jdoe@example.com', 'password1'),
+    ('Mary', 'Jane', 'mj@example.com', 'password2');
 
--- CREATE TABLE composers (
---     composer_id SERIAL PRIMARY KEY,
---     last_name VARCHAR(255) NOT NULL,
---     first_name VARCHAR(255) NOT NULL,
---     created_at TIMESTAMP DEFAULT current_timestamp,
---     updated_at TIMESTAMP DEFAULT current_timestamp
--- );
--- INSERT INTO composers ()
+INSERT INTO composers (last_name, first_name)
+VALUES
+    ('Simon', 'Paul'),
+    ('Lennon', 'John'),
+    ('McCartney', 'Paul')
+
+INSERT INTO performers (group_name, first_name, last_name)
+VALUES
+    ('Beatles', NULL, NULL),
+    (NULL, 'Paul', 'Simon');
+
+INSERT INTO songs (title, key, genre, capo, lyrics, user_id)
+VALUES
+    ('Lucy in the Sky with Diamonds', 'D', 'Rock', 3, '[A]Picture your[A7]self on a [D]boat on a [DM]river', 1),
+    ('The Boxer', NULL, 'Folk', NULL, '[C]I am just a poor boy\nThough my story''s seldom [AM]told', 1);
+
+    --     song_id SERIAL PRIMARY KEY,
+    -- title VARCHAR(255) NOT NULL,
+    -- "key" VARCHAR(5),
+    -- genre genre NOT NULL,
+    -- capo VARCHAR(20),
+    -- lyrics TEXT NOT NULL,
+    -- user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
