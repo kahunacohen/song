7ccbe172d383902e7da7e5c9973468dfc27cb5a9
Aaron Cohen <aaroncohendev@gmail.com>
2023-12-21 18:52:33 +0200

    add middeware

--- controllers/songs.go
+++ controllers/songs.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,11 +10,14 @@ import (
 func SongsByUser(c *gin.Context) {
 	userID := c.Param("user_id")
 	songID := c.Param("song_id")
-
-	response := gin.H{
-		"user_id": userID,
-		"song_id": songID,
+	if c.MustGet("rsp_fmt") == "json" {
+		response := gin.H{
+			"user_id": userID,
+			"song_id": songID,
+		}
+		// Respond with the data
+		c.JSON(http.StatusOK, response)
+	} else {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(fmt.Sprintf("<ul><li>user: %s</li><li>song: %s</li></ul>", userID, songID)))
 	}
-	// Respond with the data
-	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -4,14 +4,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kahunacohen/songs/controllers"
 )
 
+func ResponseFormatMiddleware(c *gin.Context) {
+	if strings.Contains(c.Request.URL.Path, "/api") {
+		c.Set("rsp_fmt", "json")
+	} else {
+		c.Set("rsp_fmt", "html")
+	}
+	c.Next()
+}
 func main() {
-	r := gin.Default()
+	router := gin.Default()
+	router.Use(ResponseFormatMiddleware)
+	router.GET("/api/v1/users/:user_id/:song_id", controllers.SongsByUser)
+	router.GET("/users/:user_id/:song_id", controllers.SongsByUser)
 
-	r.GET("/api/v1/users/:user_id/:song_id", controllers.SongsByUser)
-	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	router.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
