bd1d1e677bfa236ba0d1df564263413cac8feae4
Aaron Cohen <aaroncohendev@gmail.com>
2024-01-03 22:17:37 +0000

    add single song view

--- controllers/song.go
+++ controllers/song.go
@@ -13,20 +13,18 @@ import (
 
 func SongByUserHandler(conn *pgx.Conn) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		userID := c.Param("user_id")
 		songID := c.Param("song_id")
-		userIDAsInt, _ := strconv.Atoi(userID)
 		songIDAsInt, _ := strconv.Atoi(songID)
-		songs, err := db.GetSongByID(conn, songIDAsInt)
+		song, err := db.GetSongByID(conn, songIDAsInt)
 		if err != nil {
 			fmt.Println("error")
 		}
 		if c.MustGet("rsp_fmt") == "json" {
-			c.JSON(http.StatusOK, songs)
+			c.JSON(http.StatusOK, song)
 		} else {
 			templates.Render(c, templates.Base(
-				"Songs",
-				templates.Songs(userID, songs),
+				song.Title,
+				templates.Song(*song),
 			))
 		}
 	}
--- db/db.go
+++ db/db.go
@@ -36,3 +36,13 @@ func GetSongsByUser(conn *pgx.Conn, userID int) ([]Song, error) {
 	defer rows.Close()
 	return songs, nil
 }
+
+func GetSongByID(conn *pgx.Conn, id int) (*Song, error) {
+	query := "SELECT title, genre, lyrics FROM songs WHERE song_id = $1;"
+	row := conn.QueryRow(context.Background(), query, id)
+	var song Song
+	if err := row.Scan(&song.Title, &song.Genre, &song.Lyrics); err != nil {
+		return nil, fmt.Errorf("error scanning row: %v", err)
+	}
+	return &song, nil
+}
